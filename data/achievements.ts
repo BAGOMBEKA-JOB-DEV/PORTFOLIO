b13fffbc779899bb8d5a0719b1a86a354a701b94
BAGOMBEKA-JOB-DEV <bagombekajob@gmail.com>
2025-03-28 16:22:55 +0300

    Add achievements section

--- data/achievements.ts
+++ data/achievements.ts
@@ -3,28 +3,28 @@ import type { Achievement } from "types/Sections";
 const achievementList: Achievement[] = [
   {
     id: 1,
-    title: "International Flutter Hackathon '19",
-    subtitle: "1st Place",
+    title: "International Flutter Hackathon '23",
+    subtitle: "Participant",
   },
   {
     id: 2,
-    title: "Venturesity Banyan Hack '16",
-    subtitle: "1st Place",
+    title: "Sail Institute of Science & Technology Hackathon '21",
+    subtitle: "Participant",
   },
   {
     id: 3,
-    title: "eGovernments Foundation Hackathon '17",
-    subtitle: "1st Place",
+    title: "eGovernments Foundation Hackathon '24",
+    subtitle: "Participant",
   },
   {
     id: 4,
-    title: "Smart India Hackathon '17",
-    subtitle: "1st Place",
+    title: "Smart Uganda Hackathon '24",
+    subtitle: "Participant",
   },
   {
     id: 5,
-    title: "NIT Trichy - Pragyan Hackathon '16",
-    subtitle: "1st Place",
+    title: "Pycorn Uganda Hackathon '25",
+    subtitle: "Participant",
   },
 ];
 
