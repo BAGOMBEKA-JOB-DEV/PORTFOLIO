141d89d76714709833120eb91d723a83ea953b79
BAGOMBEKA-JOB-DEV <bagombekajob@gmail.com>
2025-03-28 16:54:10 +0300

    Add TAWK_API for live messaging

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,21 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
-        <title>Bagombeka Job - My Portofolio</title>
+        <title>Bagombeka Job - My Portfolio</title>
+        {/* Tawk.to Script */}
+        <script type="text/javascript">
+          {`
+            var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+            (function() {
+              var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+              s1.async = true;
+              s1.src = 'https://embed.tawk.to/67c5c24b9809b71907145924/1ile7u0gj';
+              s1.charset = 'UTF-8';
+              s1.setAttribute('crossorigin', '*');
+              s0.parentNode.insertBefore(s1, s0);
+            })();
+          `}
+        </script>
       </Head>
 
       <ThemeProvider>
