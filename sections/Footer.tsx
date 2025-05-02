8dbc45786cda091a5d58c0fb452b40eda767aab6
BAGOMBEKA-JOB-DEV <bagombekajob@gmail.com>
2025-05-02 19:22:56 +0300

    Update: Increase the text opacity

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -1,7 +1,7 @@
 import { format } from "date-fns";
 
 const Footer = () => (
-  <div id="footer" className="mb-16 text-xs leading-loose opacity-50">
+  <div id="footer" className="mb-16 text-xs leading-loose opacity-70">
     Please Ensure to give me credit incase you fork or clone this project on <strong>GITHUB</strong>.Thank You!
     <br />
     Copyright {format(Date.now(), "yyyy")} Bagombeka Job
