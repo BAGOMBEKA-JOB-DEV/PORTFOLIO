e59cafee8b7d7412f670da868406b871ab49c81b
BAGOMBEKA-JOB-DEV <bagombekajob@gmail.com>
2025-05-14 17:48:05 +0300

    Add resume file path

--- data/links.ts
+++ data/links.ts
@@ -3,7 +3,7 @@ const links = {
   linkedin: "https://www.linkedin.com/in/bagombeka-job/",
   twitter: "https://www.twitter.com/job_bags",
   github: "https://www.github.com/BAGOMBEKA-JOB-DEV",
-  resume: "https://bagombekajob.com/resume-pdf",
+  resume: "/images/resume/Bagombeka_Job's_resume.pdf",
   repository: "https://github.com/BAGOMBEKA-JOB-DEV/PORTOFOLIO",
   dribbble:"https://dribbble.com/jobbags",
   dev: "https://dev.to/bagombeka_job",
