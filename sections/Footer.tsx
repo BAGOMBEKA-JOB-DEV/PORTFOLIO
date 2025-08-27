58a8e4732ffe2cb684648c204b1a80dec0799b45
BAGOMBEKA-JOB-DEV <bagombekajob@gmail.com>
2025-08-27 15:38:16 +0300

    Update footer

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -1,10 +1,19 @@
 import { format } from "date-fns";
 
 const Footer = () => (
-  <div id="footer" className="mb-16 text-xs leading-loose opacity-70">
-    Follow me on  <strong>GITHUB</strong>.Thank You!
+  <div id="footer" className="mb-16 text-xs leading-loose opacity-80 ">
+    Thank you for visiting my website!
     <br />
-    Copyright {format(Date.now(), "yyyy")} Bagombeka Job
+    Follow me on{" "}
+    <strong className="font-bold">
+      <a className="text-blue-600 font-bold italic" href="https://www.github.com/BAGOMBEKA-JOB-DEV">
+        {" "}
+        GITHUB
+      </a>
+    </strong>{" "}
+    to see my latest projects, updates, and open-source contributions. Your support and feedback mean a lot!
+    <br />
+    All content is © {format(Date.now(), "yyyy")} Bagombeka Job. <i className="text-green-500">See you soon!</i>.
   </div>
 );
 
